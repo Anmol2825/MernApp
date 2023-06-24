27e29749c18d994bf432a90d5cbdbfd30d78f232
Anmol Agarwal <anmol@Anmols-Air.alphionsee.in>
2023-06-24 17:12:25 +0530

    Adding Signup and Login

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import { Fragment } from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+
+function App() {
+  return (
+    <Router>
+      <Fragment>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Landing />} />
+        </Routes>
+        <section className="container">
+          <Routes>
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </section>
+      </Fragment>
+    </Router>
+  );
+}
+
+export default App;
